bd6b062193c06f8d1adc4a52efb357a750304a6c
Ratchanon Yaemboo <toonkaf@gmail.com>
2026-02-10 01:17:04 +0700

    cell04

---
+++ cell04/ex03/float.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+num = float(input("Give me number: "))
+if (num.is_integer()):
+	print("This number is an integer.")
+else:
+	print("This number is a decimal.")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import math
+num = math.ceil(float(input("Give me a number: ")))
+print(num)
